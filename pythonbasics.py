335bd13f04099972b1f08fc7f467021548ebccaa
SapheraFireheart13 <149187582+SapheraFireheart13@users.noreply.github.com>
2023-11-10 12:35:10 -0500

    lalala

--- pythonbasics.py
+++ pythonbasics.py
@@ -1,2 +1,20 @@
-thisList=["abc", 34, True, 40, "male"] 
-print("Items from 1 to 3:", thisList[1] + thisList[3]) 
+nameList=["Marisol", "unicorn", "Eragon", "poop", "Good Arina"] 
+nameList.append("43") 
+nameList.insert(0,False) 
+nameList.remove("poop") 
+nameList.pop(5) 
+print(nameList) 
+x=---5
+if x>0: 
+    print ("positive") 
+elif x<0: 
+    print ("negative") 
+else :
+    print ("Zero") 
+ r=68 
+ p=56 
+if r>p:
+     print ("r to greater") 
+else r<p: 
+    print ("p to greater")
+
