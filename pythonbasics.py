306d2bb2458a6e4c491bf3ebe83fa9c01dfdda0a
SapheraFireheart13 <149187582+SapheraFireheart13@users.noreply.github.com>
2023-11-24 12:29:11 -0500

    Loops

--- pythonbasics.py
+++ pythonbasics.py
@@ -1,20 +1,12 @@
-nameList=["Marisol", "unicorn", "Eragon", "poop", "Good Arina"] 
-nameList.append("43") 
-nameList.insert(0,False) 
-nameList.remove("poop") 
-nameList.pop(5) 
-print(nameList) 
-x=---5
-if x>0: 
-    print ("positive") 
-elif x<0: 
-    print ("negative") 
-else :
-    print ("Zero") 
- r=68 
- p=56 
-if r>p:
-     print ("r to greater") 
-else r<p: 
-    print ("p to greater")
+I = 1
+number=0  
+while I <= 100: 
+    number+=I 
+    I += 1 
+print (number)
+  
+ 
+     
+
+
 
